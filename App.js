7639b2e0f3a620cc56fdcf106d10ecdd1ea8faf0
Megha <mohadikarmegha0@gmail.com>
2024-05-09 20:01:27 +0530

    episode-3

--- App.js
+++ App.js
@@ -8,10 +8,11 @@ root.render(heading);
 */
 
 const heading = React.createElement(
-    "h1",
-    {id : "attributeOfTheTag", xyz:"abc"}, 
-    "Hello World with ReactJS!!!!");
-console.log(heading); //object 
+  'h1',
+  { id: 'attributeOfTheTag', xyz: 'abc' },
+  'Hello World with ReactJS!!!!'
+);
+console.log(heading); //object
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading); //converts heading object to h1 html tag
 
@@ -28,21 +29,58 @@ root.render(heading); //converts heading object to h1 html tag
 </div›
 ReactElement (Object) = HTML (Browser Understands)
 */
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement(
-        "div", 
-        {id: "child1" }, 
-        [React. createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag")]
-    ),
-    React.createElement(
-        "div", 
-        {id: "child2" }, 
-        [React. createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag")]
-    )]
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1' }, [
+    React.createElement('h1', {}, "I'm an h1 tag"),
+    React.createElement('h2', {}, "I'm an h2 tag"),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, "I'm an h1 tag"),
+    React.createElement('h2', {}, "I'm an h2 tag"),
+  ]),
+]);
+console.log(parent); // object
+root.render(parent); // it will replace root div with parent
+
+//episode-3
+const reactTag = React.createElement(
+  'h1',
+  { id: 'heading' },
+  'h1 tag in core react'
 );
-console. log(parent); // object
-root. render (parent); // it will replace root div with parent
+console.log(reactTag);
+const jsxTag = <h1 id="heading">h1 tag in core react</h1>;
+console.log(jsxTag);
+
+//react functional component
+//in arrow function, if method body is 1 liner then you can skip return keyword & {}
+const Heading = () => <h1> React functional component</h1>;
+const number = 1000;
+//component composition
+/* const Title = () => (
+  <div>
+    <h2 id="title" className="head">
+      Title for functional component
+    </h2>
+    ;
+    <Heading />
+  </div>
+); */
+
+const Title = function () {
+  return (
+    <div>
+      <h2 id="title" className="head">
+        Title for functional component
+      </h2>
+      <Heading />
+      <Heading></Heading>
+      {Heading()}
+      {number + 10}
+      {heading}
+
+      <h3>{console.log(number)}</h3>
+    </div>
+  );
+};
+root.render(<Title />);
