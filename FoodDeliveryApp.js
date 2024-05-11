7edf0c15e8c0bfe602700ee8992805378e4aa579
Megha <mohadikarmegha0@gmail.com>
2024-05-11 19:26:50 +0530

    addition file in previous commit

---
+++ FoodDeliveryApp.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCard
+ *      - Img
+ *      - Name of Res, Star Rating, cuisine, delevery tie
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://th.bing.com/th/id/OIP.yLpyUoQ9Qz3hYB47Pdi1pgHaHa?rs=1&pid=ImgDetMain"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">SearchBar</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-img"
+        alt="res-img"
+        src="https://th.bing.com/th/id/OIP.XY4MxQqcOZC7M4R-i33UZAHaGm?rs=1&pid=ImgDetMain"
+      ></img>
+      <h3>KFC</h3>
+      <h4>Fried Chicken, Biryani Bowl</h4>
+      <h4>4.8 stars</h4>
+      <h4>45 mins</h4>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />);
